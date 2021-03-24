f2b9099f7bc05d84215682748163c174142800da
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-03-24 14:54:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+import os
+
+from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
+
+
+class MainForm(QMainWindow):
+	def __init__(self):
+		super(MainForm, self).__init__()
+		self.load_ui()
+
+	def load_ui(self):
+		loader = QUiLoader()
+		path = os.path.join(os.path.dirname(__file__), "MainForm.ui")
+		ui_file = QFile(path)
+		ui_file.open(QFile.ReadOnly)
+		loader.load(ui_file, self)
+		ui_file.close()
+
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	w = QWidget()
-	w.resize(250, 150)
-	w.move(300, 300)
+	w = MainForm()
+	# w.resize(250, 150)
+	# w.move(300, 300)
 	w.setWindowTitle('Simple')
 	w.show()
 	sys.exit(app.exec_())
