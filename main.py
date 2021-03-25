7ff8e606edfa34fdb48aab67e39732c62d1682b4
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-03-25 11:15:08 +0800

    initial ver

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PySide2.QtWidgets import QApplication, QMainWindow
 from Ui_MainWindow import Ui_MainWindow
 
 
-class MainWindow(QMainWindow, Ui_MainWindow):
+class MainWindow(Ui_MainWindow):
 	def __init__(self):
 		super(MainWindow, self).__init__()
 		self.setupUi(self)
