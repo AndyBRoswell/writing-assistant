1b547b0d94697cb945fba63880855f25c8590796
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-03-25 12:20:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import QObject, Signal, Slot
 from Ui_MainWindow import Ui_MainWindow
 
 
@@ -11,7 +12,18 @@ class MainWindow(Ui_MainWindow):
 		self.show()
 
 
+@Slot(int, str)
+def slot():
+	print("Slot function has been called.")
+
+
+class Action:
+	signal = Signal(int, str)
+
+
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 	gui = MainWindow()
+	action = Action()
+	action.signal.connect(slot)
 	sys.exit(app.exec_())
