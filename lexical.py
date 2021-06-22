c4df8e48d1e0462d0e0af039922311a4d52f3622
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-06-22 19:53:18 +0800

    /

--- lexical.py
+++ lexical.py
@@ -1,9 +1,13 @@
+import jiagu
+import jionlp
 import synonyms
+import textrank4zh
 
 
 def get_synonyms(word, count = 1):
-	return synonyms.nearby(word, count)
+	return synonyms.nearby(word, count)[0]
 
 
 def tokenize(text):
-	return synonyms.seg(text)
+	# return synonyms.seg(text)[0]
+	return jiagu.seg(text)
