eeb1d867f4130267a50e8742640de6f54df16a8c
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-06-23 11:07:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,19 +48,23 @@ if __name__ == '__main__':
 		print("synonyms:" + globals.linesep)
 		for row in rows:
 			print(synonyms.nearby(''.join(row), 16))
-	print("globals.linesep + ================ End Synonyms ================" + globals.linesep)
+	print(globals.linesep + "================ End Synonyms ================" + globals.linesep)
 	
+	print(globals.linesep + "================ Lexical & Semantic ================" + globals.linesep)
 	with open("paragraphs.csv") as paragraphs_file:
 		entity_list = json.loads("entity-list.json")
 		rows = csv.reader(paragraphs_file)
 		for row in rows:
 			text = ''.join(row)
+			print("text: " + globals.linesep + text)
+			
 			# word seg
 			print(globals.linesep + "================ Word Seg ================" + globals.linesep)
 			print("jiagu:" + globals.linesep)
 			print(jiagu.seg(text))
 			print("synonyms:" + globals.linesep)
 			print(synonyms.seg(text))
+			
 			# entity recognition
 			print(globals.linesep + "================ Entity Recognition ================" + globals.linesep)
 			print("jiagu:" + globals.linesep)
@@ -68,13 +72,21 @@ if __name__ == '__main__':
 			print("jionlp:" + globals.linesep)
 			entity_recognizer = jionlp.ner.LexiconNER(entity_list)
 			print(entity_recognizer(text))
+			
 			# extract / generate keywords
 			print(globals.linesep + "================ Keywords ================" + globals.linesep)
+			keyword_count = 16
 			print("jiagu:" + globals.linesep)
-			print(jiagu.keywords(text))
+			print(jiagu.keywords(text, keyword_count))
 			print("synonyms:" + globals.linesep)
-			print(synonyms.keywords(text))
+			print(synonyms.keywords(text, keyword_count))
+			
 			# extract / generate summary
+			print(globals.linesep + "================ Summaries ================" + globals.linesep)
+			print("jiagu:" + globals.linesep)
+			print(jiagu.summarize(text))
+			print("jionlp:" + globals.linesep)
+			print(jionlp.summary.extract_summary(text))
 			
 			# sentimental analysis
 			
