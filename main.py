37bb86064530f7ec1ff07c3859f8d9d1e0532e61
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-06-23 11:02:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ if __name__ == '__main__':
 	print(globals.linesep + "================ Synonyms ================" + globals.linesep)
 	with open("synonyms.csv") as synonyms_file:
 		rows = csv.reader(synonyms_file)
+		print("synonyms:" + globals.linesep)
 		for row in rows:
 			print(synonyms.nearby(''.join(row), 16))
 	print("globals.linesep + ================ End Synonyms ================" + globals.linesep)
@@ -56,17 +57,23 @@ if __name__ == '__main__':
 			text = ''.join(row)
 			# word seg
 			print(globals.linesep + "================ Word Seg ================" + globals.linesep)
-			print(synonyms.seg(text))
+			print("jiagu:" + globals.linesep)
 			print(jiagu.seg(text))
+			print("synonyms:" + globals.linesep)
+			print(synonyms.seg(text))
 			# entity recognition
 			print(globals.linesep + "================ Entity Recognition ================" + globals.linesep)
+			print("jiagu:" + globals.linesep)
 			print(jiagu.ner(''.join(row)))
+			print("jionlp:" + globals.linesep)
 			entity_recognizer = jionlp.ner.LexiconNER(entity_list)
 			print(entity_recognizer(text))
 			# extract / generate keywords
 			print(globals.linesep + "================ Keywords ================" + globals.linesep)
+			print("jiagu:" + globals.linesep)
+			print(jiagu.keywords(text))
+			print("synonyms:" + globals.linesep)
 			print(synonyms.keywords(text))
-			
 			# extract / generate summary
 			
 			# sentimental analysis
