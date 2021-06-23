452c12e2a769965f0587e91c76cd2d26d8dec091
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-06-23 22:29:47 +0800

    /

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 		rows = csv.reader(synonyms_file)
 		print(globals.linesep + "synonyms:")
 		for row in rows:
-			print(synonyms.nearby(''.join(row), 16))
+			print('、'.join(synonyms.nearby(''.join(row), 16)[0]))
 	print(globals.linesep + "================ End Synonyms ================" + globals.linesep)
 	
 	print(globals.linesep + "================ Lexical & Semantic ================" + globals.linesep)
