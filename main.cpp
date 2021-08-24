014f56c72f23566e4b3794651008ee13916e2f6c
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-08-24 11:05:33 +0800

    rearranged files

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
+#include <semaphore>
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <QLocale>
+#include <QTranslator>
+
 int main(int argc, char *argv[]) {
-#if defined(Q_OS_WIN)
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-
+	
 	QGuiApplication app(argc, argv);
-
+	
+	QTranslator translator;
+	const QStringList uiLanguages = QLocale::system().uiLanguages();
+	for (const QString &locale : uiLanguages) {
+		const QString baseName = "writing-assistant_" + QLocale(locale).name();
+		if (translator.load(":/i18n/" + baseName)) {
+			app.installTranslator(&translator);
+			break;
+		}
+	}
+	
 	QQmlApplicationEngine engine;
-	engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-	if (engine.rootObjects().isEmpty()) return -1;
-
+	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
+					 [url](QObject *obj, const QUrl &objUrl) {
+		if (!obj && url == objUrl) QCoreApplication::exit(-1);
+	}, Qt::QueuedConnection);
+	engine.load(url);
+	
 	return app.exec();
 }
--- main.qml
+++ main.qml
@@ -1,9 +1,10 @@
 import QtQuick
+import QtQuick.Controls
 import QtQuick.Window
 
 Window {
-	visible: true
 	width: 640
 	height: 480
+	visible: true
 	title: qsTr("写作助手")
 }
